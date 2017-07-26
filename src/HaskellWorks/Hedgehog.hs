0fd750bbbf733045027e1bc4a61f2b0df63591c7
John Ky <newhoggy@gmail.com>
2017-07-27 08:14:05 +1000

    Initial project

---
+++ src/HaskellWorks/Hedgehog.hs
@@ -0,0 +1,26 @@
+module HaskellWorks.Hedgehog where
+
+import Hedgehog
+import Hedgehog.Internal.Property (MonadTest(..), failDiff, failWith)
+import Hedgehog.Internal.Source (HasCallStack(..), withFrozenCallStack)
+import Hedgehog.Internal.Show
+
+reversed :: Group -> Group
+reversed (Group name properties) = Group name (reverse properties)
+
+(/==) :: (MonadTest m, Eq a, Show a, HasCallStack) => a -> a -> m ()
+(/==) x y = do
+  ok <- withFrozenCallStack $ eval (x /= y)
+  if ok
+    then success
+    else withFrozenCallStack $ failDiff x y
+
+(?==) :: (MonadTest m, Eq a, Show a, HasCallStack) => a -> (a -> Bool) -> m ()
+(?==) x p = do
+  r <- withFrozenCallStack $ eval x
+  if p r
+    then success
+    else withFrozenCallStack $ failWith Nothing $ unlines
+          [ "━━━ Invalid result ━━━"
+          , showPretty x
+          ]

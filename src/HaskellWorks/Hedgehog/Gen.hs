273d15a0ebd1cd02f4633406bdd5c61f1a099ff4
John Ky <newhoggy@gmail.com>
2018-07-20 23:06:14 +1000

    Add tuple support

--- src/HaskellWorks/Hedgehog/Gen.hs
+++ src/HaskellWorks/Hedgehog/Gen.hs
@@ -4,6 +4,14 @@ module HaskellWorks.Hedgehog.Gen
   ( vector
   , storableVector
   , word8x8
+  , tuple2
+  , tuple3
+  , tuple4
+  , tuple5
+  , tuple6
+  , tuple7
+  , tuple8
+  , tuple9
   ) where
 
 import Data.Bits
@@ -40,3 +48,27 @@ word8x8 gen = do
             (f `shiftL` 16) .|.
             (g `shiftL`  8) .|.
             (h `shiftL`  0)
+
+tuple2 :: MonadGen m => m a -> m (a, a)
+tuple2 gen = (,) <$> gen <*> gen
+
+tuple3 :: MonadGen m => m a -> m (a, a, a)
+tuple3 gen = (,,) <$> gen <*> gen <*> gen
+
+tuple4 :: MonadGen m => m a -> m (a, a, a, a)
+tuple4 gen = (,,,) <$> gen <*> gen <*> gen <*> gen
+
+tuple5 :: MonadGen m => m a -> m (a, a, a, a, a)
+tuple5 gen = (,,,,) <$> gen <*> gen <*> gen <*> gen <*> gen
+
+tuple6 :: MonadGen m => m a -> m (a, a, a, a, a, a)
+tuple6 gen = (,,,,,) <$> gen <*> gen <*> gen <*> gen <*> gen <*> gen
+
+tuple7 :: MonadGen m => m a -> m (a, a, a, a, a, a, a)
+tuple7 gen = (,,,,,,) <$> gen <*> gen <*> gen <*> gen <*> gen <*> gen <*> gen
+
+tuple8 :: MonadGen m => m a -> m (a, a, a, a, a, a, a, a)
+tuple8 gen = (,,,,,,,) <$> gen <*> gen <*> gen <*> gen <*> gen <*> gen <*> gen <*> gen
+
+tuple9 :: MonadGen m => m a -> m (a, a, a, a, a, a, a, a, a)
+tuple9 gen = (,,,,,,,,) <$> gen <*> gen <*> gen <*> gen <*> gen <*> gen <*> gen <*> gen <*> gen
